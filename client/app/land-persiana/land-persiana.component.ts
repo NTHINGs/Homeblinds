78439c4358f2c699c8611021fe442fc8e0151e2d
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-09 11:21:01 -0500

    Update land-persiana.component.ts

--- client/app/land-persiana/land-persiana.component.ts
+++ client/app/land-persiana/land-persiana.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
+
 // Component decorator
 @Component({
   selector: 'land-persiana',
@@ -9,22 +10,18 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 // Any method defined here is accesible by the template
 export class LandPersianaComponent implements OnInit {
-    @Input() nombre: string;
+    @Input() persiana: any;
     @Input() offset: number;
     @Output() cambioPersiana = new EventEmitter();
-
-    emitirNombre(){
-      this.cambioPersiana.emit(this.nombre);
-    };
-
-    // TODO: getting this from service
-    images = [
-      "../assets/img/bg2.jpeg",
-      "../assets/img/bg3.jpeg",
-      "../assets/img/bg4.jpeg"
-    ];
+    
     // In the constructor is defined dependency injection
     constructor(){ }
     // Everything in this method would run at init
-    ngOnInit(){ }
+    ngOnInit(){ 
+      
+    }
+
+    emitirNombre(){
+      this.cambioPersiana.emit(this.persiana);
+    };
 }
