c2cf89d69568b3a23e7131a76f7dfb24bc39f8dc
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-09 12:25:28 -0500

    Update persiana.ts

--- server/controllers/persiana.ts
+++ server/controllers/persiana.ts
@@ -3,4 +3,16 @@ import BaseCtrl from './base';
 
 export default class PersianaCtrl extends BaseCtrl {
   model = Persiana;
+
+  // Hardcoded meanwhile mongo is set
+  getAll = (req, res) => {
+    let persianas = [
+      {
+        name:"enrollable", 
+        descripcion:"Esto es una prueba", 
+        galeria: [{foto: "../assets/img/bg2.jpeg"}, {foto: "../assets/img/bg3.jpeg"}, {foto: "../assets/img/bg4.jpeg"}]
+      }
+    ];
+    res.json(persianas);
+  }
 }
