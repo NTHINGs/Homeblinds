46c1b302c8d246e238a83ac258e5b08c4bff23e0
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-11 09:26:40 -0500

    Update app.component.ts

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {PageScrollConfig} from 'ng2-page-scroll';
+import { PageScrollConfig } from 'ng2-page-scroll';
 
 // Component decorator
 @Component({
@@ -10,6 +10,7 @@ import {PageScrollConfig} from 'ng2-page-scroll';
 
 // Any method defined here is accesible by the template
 export class AppComponent {
+  admin = true;
   constructor() {
       PageScrollConfig.defaultScrollOffset = 80;
       PageScrollConfig.defaultDuration = 500;
