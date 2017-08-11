1c9bf2da4eb6bd795be15bd944e9a3de16d7930c
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-11 15:10:30 -0500

    Update app.module.ts

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { Ng2PageScrollModule } from 'ng2-page-scroll';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import { MODULE_COMPONENTS, MODULE_ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { LandPageComponent } from './land-page/land-page.component';
@@ -12,6 +11,7 @@ import { ItemComponent } from './item/item.component';
 import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
+import { DashboardModule } from './admin/dashboard/dashboard.module';
 import { SidebarModule } from './admin/sidebar/sidebar.module';
 import { FooterModule } from './admin/shared/footer/footer.module';
 import { NavbarModule} from './admin/shared/navbar/navbar.module';
@@ -21,21 +21,27 @@ import { PersianaService } from './services/persiana.service';
 
 @NgModule({
   declarations: [
+    
     AppComponent,
     LandPageComponent,
     LandPersianaComponent,
     ItemComponent,
     AdminComponent,
-    MODULE_COMPONENTS
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     Ng2PageScrollModule,
     HttpModule,
+    DashboardModule,
     SidebarModule,
     NavbarModule,
     FooterModule,
-    RouterModule.forRoot([MODULE_ROUTES])
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'landing', pathMatch: 'full' },
+      { path: 'landing', component: AppComponent },
+      { path: 'admin', component: DashboardComponent }
+    ])
   ],
   providers: [
     PersianaService
