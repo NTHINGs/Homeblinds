b0ddd7b5ad451f8525896d698d7d52482c979271
Mauricio Alejandro Martínez Pacheco <n_othing@hotmail.com>
2017-08-11 15:06:02 -0500

    Update app.module.ts

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { Ng2PageScrollModule } from 'ng2-page-scroll';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { MODULE_COMPONENTS, MODULE_ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
 import { LandPageComponent } from './land-page/land-page.component';
@@ -11,7 +12,6 @@ import { ItemComponent } from './item/item.component';
 import { AdminComponent } from './admin/admin.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
-import { DashboardModule } from './admin/dashboard/dashboard.module';
 import { SidebarModule } from './admin/sidebar/sidebar.module';
 import { FooterModule } from './admin/shared/footer/footer.module';
 import { NavbarModule} from './admin/shared/navbar/navbar.module';
@@ -26,17 +26,16 @@ import { PersianaService } from './services/persiana.service';
     LandPersianaComponent,
     ItemComponent,
     AdminComponent,
-    DashboardComponent
+    MODULE_COMPONENTS
   ],
   imports: [
     BrowserModule,
     Ng2PageScrollModule,
     HttpModule,
-    DashboardModule,
     SidebarModule,
     NavbarModule,
     FooterModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([MODULE_ROUTES])
   ],
   providers: [
     PersianaService
